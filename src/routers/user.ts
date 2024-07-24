f085fd8670fd064f8928e05339c10b8e22db2966
marcospeciale1 <marcospeciale16@gmail.com>
2024-07-24 16:11:28 +0200

    docs(exercise): create api users

---
+++ src/routers/user.ts
@@ -0,0 +1,101 @@
+import { createClient } from "@vercel/postgres";
+import express, { Response, Request } from "express";
+import { config } from "dotenv";
+import "dotenv/config";
+
+const client = createClient({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const routerUser = express.Router();
+
+routerUser.post("/api/auth/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+routerUser.post("/api/auth/admin/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+routerUser.post("/api/auth/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  client.query(
+    "SELECT * FROM users WHERE email = $1 AND password = $2",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "logged in",
+        });
+      }
+    }
+  );
+});
+
+routerUser.get("/api/auth/logout", (req: Request, res: Response) => {
+  const { id } = req.body;
+  client.query(
+    "DELETE FROM auth_token WHERE user_id = $1",
+    [id],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "logged out",
+        });
+      }
+    }
+  );
+});
+
+routerUser.get("/api/auth/user", (req: Request, res: Response) => {
+  const { id } = req.body;
+  client.query("SELECT * FROM users WHERE id = $1", [id], (error, response) => {
+    res.json(response.rows);
+  });
+});
+
+routerUser.get("/api/auth/users", (req: Request, res: Response) => {
+  client.query("SELECT * FROM users", (error, response) => {
+    res.json(response.rows);
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@vercel/postgres";
 import express, { Response, Request } from "express";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 /*****************************
  *                           *
  *  INIZIALIZZAZIONE SERVER  *
@@ -8,8 +8,8 @@ import { config } from 'dotenv';
  *****************************/
 config();
 
-const port = process.env.PORT || 3000
-const baseURL =  "http://localhost"
+const port = process.env.PORT || 3000;
+const baseURL = "http://localhost";
 // const url_db = process.env.BASE_URL_DB;
 
 // creiamo un applicazione express
@@ -17,11 +17,13 @@ const app = express();
 //istanziare un server
 const server = express.json();
 // inizializziamo un server
-app.use(server)
+app.use(server);
 
 const client = createClient({
-    connectionString: process.env.DATABASE_URL,
-  });
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
 
 /*****************************
  *                           *
@@ -36,14 +38,113 @@ const client = createClient({
  *****************************/
 
 /*-------------------------------------------- */
+
+/*****************************
+ *       API USERS           *
+ *****************************/
+
+app.post("/api/auth/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+app.post("/api/auth/admin/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+app.post("/api/auth/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  client.query(
+    "SELECT * FROM users WHERE email = $1 AND password = $2",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "logged in",
+        });
+      }
+    }
+  );
+});
+
+app.get("/api/auth/logout", (req: Request, res: Response) => {
+  const { id } = req.body;
+  client.query(
+    "DELETE FROM auth_token WHERE user_id = $1",
+    [id],
+    (err, result) => {
+      if (err) {
+        res.status(400).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: "logged out",
+        });
+      }
+    }
+  );
+});
+
+app.get("/api/auth/user", (req: Request, res: Response) => {
+  const { id } = req.body;
+  client.query("SELECT * FROM users WHERE id = $1", [id], (error, response) => {
+    res.json(response.rows);
+  });
+});
+
+app.get("/api/auth/users", (req: Request, res: Response) => {
+  client.query("SELECT * FROM users", (error, response) => {
+    res.json(response.rows);
+  });
+});
+
+/*****************************
+ *       API USERS           *
+ *****************************/
+
 /*****************************
  *       API PRODUCTS        *
  *****************************/
 
 app.get("/api/products", (req: Request, res: Response) => {
-    client.query("SELECT * FROM products")
-})
-
+  client.query("SELECT * FROM products");
+});
 
 /*****************************
  *       API PRODUCTS        *
@@ -52,31 +153,19 @@ app.get("/api/products", (req: Request, res: Response) => {
 /*****************************
  *       API CART            *
  *****************************/
-app.get("/api/carts",(req: Request, res:Response) =>{
-})
-
-
+app.get("/api/carts", (req: Request, res: Response) => {});
 
 /*****************************
  *       API CART            *
  *****************************/
 
-
-
 /*****************************
  *       API orders          *
  *****************************/
 
-
-
-
-
 /*****************************
  *       API orders          *
  *****************************/
-
-
-
 
 /*****************************
  *                           *
@@ -86,5 +175,5 @@ app.get("/api/carts",(req: Request, res:Response) =>{
 // inizializiamo le porte da cui runnare il server
 // PRIMO PARAMETRO PORTA SU CUI RUNNARE
 app.listen(3000, () => {
-    console.log("server is running in "+baseURL+ port)
-})
+  console.log("server is running in " + baseURL + port);
+});
