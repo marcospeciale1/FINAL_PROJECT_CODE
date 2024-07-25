ea45840ee8a18c040b9dd74813cce5899894e571
marcospeciale1 <marcospeciale16@gmail.com>
2024-07-25 14:04:52 +0200

    fix user api

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@vercel/postgres";
 import express, { Response, Request } from "express";
 import "dotenv/config";
-import { authenticateToken, generateAccessToken } from "../JWT/authenticateToken";
+import {
+  authenticateToken,
+  generateAccessToken,
+} from "../JWT/authenticateToken";
 
 const client = createClient({
   connectionString: process.env.DATABASE_URL,
@@ -50,9 +53,8 @@ routerUser.post("/api/auth/admin/register", (req: Request, res: Response) => {
 });
 
 routerUser.post("/api/auth/login", (req: Request, res: Response) => {
-  
   const { email, password } = req.body;
-  
+
   client.query(
     "SELECT * FROM users WHERE email = $1 AND password = $2",
     [email, password],
@@ -62,40 +64,64 @@ routerUser.post("/api/auth/login", (req: Request, res: Response) => {
           message: err.message,
         });
       } else {
-        const token = generateAccessToken({ user:result.rows[0] });
-        res.status(200).json(token);
+        const token = generateAccessToken({ user: result.rows[0] });
+        client.query(
+          "INSERT INTO auth (userid,token) VALUES ($1,$2)",
+          [result.rows[0].id, token],
+          (err, result) => {
+            if (err) {
+              res.status(400).json({
+                message: err.message,
+              });
+            } else {
+              res.status(200).json(token);
+            }
+          }
+        );
       }
     }
   );
 });
 
-routerUser.get("/api/auth/logout", authenticateToken, (req: Request, res: Response) => {
+routerUser.get(
+  "/api/auth/logout",
+  authenticateToken,
+  (req: Request, res: Response) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  const { id } = req.body;
-  
-  client.query(
-    "DELETE FROM auth_token WHERE user_id = $1",
-    [id],
-    (err, result) => {
-      if (err) {
-        res.status(400).json({
-          message: err.message,
-        });
-      } else {
-        res.status(200).json({
-          message: "logged out",
-        });
+    client.query(
+      "DELETE FROM auth WHERE token = $1",
+      [token],
+      (err, result) => {
+        if (err) {
+          res.status(400).json({
+            message: err.message,
+          });
+        } else {
+          res.status(200).json({
+            message: "logged out",
+          });
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
 
-routerUser.get("/api/auth/user", (req: Request, res: Response) => {
-  const { id } = req.body;
-  client.query("SELECT * FROM users WHERE id = $1", [id], (error, response) => {
-    res.json(response.rows);
-  });
-});
+routerUser.get(
+  "/api/auth/user/:id",
+  authenticateToken,
+  (req: Request, res: Response) => {
+    const { id } = req.params;
+    client.query(
+      "SELECT * FROM users WHERE id = $1",
+      [id],
+      (error, response) => {
+        res.json(response.rows);
+      }
+    );
+  }
+);
 
 routerUser.get("/api/auth/users", (req: Request, res: Response) => {
   client.query("SELECT * FROM users", (error, response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { createClient } from "@vercel/postgres";
 import express, { Response, Request } from "express";
 import { config } from "dotenv";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import {
+  generateAccessToken,
+  authenticateToken,
+} from "./JWT/authenticateToken";
+import { log } from "console";
 
 /*****************************
  *                           *
@@ -29,7 +33,6 @@ const client = createClient({
 
 client.connect();
 
-
 /*****************************
  *                           *
  *  INIZIALIZZAZIONE SERVER  *
@@ -42,6 +45,122 @@ client.connect();
  *       API USERS           *
  *****************************/
 
+app.post("/api/auth/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).json({
+          message: err.message,
+        });
+      } else {
+        res.status(200).json({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+app.post("/api/auth/admin/register", (req: Request, res: Response) => {
+  const { email, username, password, firstname, lastname, citta, indirizzo } =
+    req.body;
+  client.query(
+    "INSERT INTO users (email, username, password, firstname, lastname, citta, indirizzo) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+    [email, username, password, firstname, lastname, citta, indirizzo],
+    (err, result) => {
+      if (err) {
+        res.status(400).json({
+          message: err.message,
+        });
+      } else {
+        res.status(200).json({
+          message: "registered",
+        });
+      }
+    }
+  );
+});
+
+app.post("/api/auth/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  client.query(
+    "SELECT * FROM users WHERE email = $1 AND password = $2",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        res.status(400).json({
+          message: err.message,
+        });
+      } else {
+        const token = generateAccessToken({ user: result.rows[0] });
+        client.query(
+          "INSERT INTO auth (userid,token) VALUES ($1,$2)",
+          [result.rows[0].id, token],
+          (err, result) => {
+            if (err) {
+              res.status(400).json({
+                message: err.message,
+              });
+            } else {
+              res.status(200).json(token);
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+app.get(
+  "/api/auth/logout",
+  authenticateToken,
+  (req: Request, res: Response) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    client.query(
+      "DELETE FROM auth WHERE token = $1",
+      [token],
+      (err, result) => {
+        if (err) {
+          res.status(400).json({
+            message: err.message,
+          });
+        } else {
+          res.status(200).json({
+            message: "logged out",
+          });
+        }
+      }
+    );
+  }
+);
+
+app.get(
+  "/api/auth/user/:id",
+  authenticateToken,
+  (req: Request, res: Response) => {
+    const { id } = req.params;
+    client.query(
+      "SELECT * FROM users WHERE id = $1",
+      [id],
+      (error, response) => {
+        res.json(response.rows);
+      }
+    );
+  }
+);
+
+app.get("/api/auth/users", (req: Request, res: Response) => {
+  client.query("SELECT * FROM users", (error, response) => {
+    res.json(response.rows);
+  });
+});
 
 /*****************************
  *       API USERS           *
@@ -50,7 +169,6 @@ client.connect();
 /*****************************
  *       API PRODUCTS        *
  *****************************/
-
 
 /*****************************
  *       API PRODUCTS        *
